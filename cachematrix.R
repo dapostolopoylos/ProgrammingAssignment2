3731890b925295b51e140d2fa876c31746adba90
Demetrios Apostolopoylos <dapostolopoylos@gmail.com>
2015-02-20 23:24:25 +0200

    ProgrammingAssignement2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## I tried to port the logic behind the Vector Mean example into the exercise tweeking the code in order to fit in the demand 
+## of the caching an inversed matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    get<-function() x
+    setinverse<-function(solve) m <<- solve
+    getinverse<-function() m
+    list(set=set, get=get,
+         setinverse=setinverse,
+         getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m<-x$getinverse()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix)
+    x$setinverse(m)
+    m
 }
+
+
+
+
